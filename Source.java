b6e770dec52c2843310b2b81a187b66ba7bcb14b
Work <personal@yash-IdeaPad-Gaming-3-15IMH05>
2023-07-12 18:13:33 +0530

    Iniital Commit

---
+++ Source.java
@@ -0,0 +1,10 @@
+package First;
+
+public class Source{
+    public int addNum(int a, int b){
+        return a+b;
+    }
+    public static void main(){
+        System.out.print("Hello from Source");
+    }
+}
---
+++ Test.java
@@ -0,0 +1,12 @@
+import First.Source;
+
+public class Test{
+
+    public static void main(String[] args){
+        Source s = new Source();
+        if(s.addNum(2, 2) == 4)
+            System.out.println("Test Case Passed");
+    }
+    
+
+}
