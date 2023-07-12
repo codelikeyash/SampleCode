3d620189fbd89d727d24f689425be252c267ad81
Work <personal@yash-IdeaPad-Gaming-3-15IMH05>
2023-07-12 18:53:57 +0530

    adds test to make sure 3+3 not eql 7

--- Test.java
+++ Test.java
@@ -3,7 +3,9 @@ public class Test extends Source{
     public static void main(String[] args){
         Source s = new Source();
         if(s.addNum(2, 2) == 4)
-            System.out.println("Test Case Passed");
+            System.out.println("Test Case Passed\n");
+        if(s.addNum(3, 3) == 7)
+            System.out.println("Test Case Failed\n");
     }
     
 
